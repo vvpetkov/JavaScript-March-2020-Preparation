0bd7053a50e6c34311660ce3870cc464ad25ccf2
Владимир Петков <vladimir.v.petkov@gmail.com>
2020-01-19 20:13:14 +0200

    Create MathHELP.js

---
+++ MathHELP.js
@@ -0,0 +1,36 @@
+Facebook (JavaScript Mar'20 - Preparation)
+19.01.2020
+
+let roundedA = Math.round(5.1);       // Закръгля числото до цяло математически
+let roundedB = Math.round(5.6);       // Закръгля числото до цяло математически
+console.log(roundedA);                // 5
+console.log(roundedB);                // 6
+
+let roundedDown = Math.floor(5.55);   // Закръгля числото до цяло надолу
+console.log(roundedDown);             // 5
+
+let roundedUp = Math.ceil(5.1);       // Закръгля числото до цяло нагоре
+console.log(roundedUp);               // 6
+
+let maxNumber = Math.max(1, 2, 8, 3); // Намира най-голямото число
+console.log(maxNumber);               // 8
+
+let minNumber = Math.min(1, 2, 8, 3); // Намира най-малкото число
+console.log(minNumber);               // 1
+
+let asboluteValueA = Math.abs(5);     // Превръща числото в абсолютната му стойност 
+let asboluteValueB = Math.abs(-5);    // ако е положително си остава положително ако е отрицателно става положително
+console.log(asboluteValueA);          // 5
+console.log(asboluteValueB);          // 5
+
+let sqrt = Math.sqrt(9);              // Намира корен квадратен от дадено число
+console.log(sqrt);                    // 3
+
+let powered = Math.pow(5, 2);         // Вдига число на степен (в случая 5 на степен 2 = 25)
+console.log(powered);                 // 25
+
+let random = Math.random();           // Дава ни произволно число между 0.00 и 1.00
+console.log(random);                  // random number
+
+let pi = Math.PI;                     // Числото ПИ
+console.log(pi);                      // 3.141592653589793
