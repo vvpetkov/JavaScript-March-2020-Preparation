bf8f9fb7f7b9f088e32fe7dea187aa9d06774241
Vladimir Petkov <vladimir.v.petkov@gmail.com>
2020-01-21 12:30:35 +0200

    Adding folder Demos and files for 02 session

---
+++ Demos/02.Prep-Sesion.js
@@ -0,0 +1,115 @@
+
+console.log("Vladimir Petkov")
+
+// деклариране на променлива
+// let firstName;
+// оператори
+// =  присвояване на стойност
+// +  конкатенация
+// 
+// integer - цяло число
+// парсване
+// модулно деление 
+// Закоментиране                 
+
+
+
+
+let firstName = 'Vladimir';
+let lastName = 'Pekov';
+let age = 99;
+age = age + 1;
+age += 1.5
+age -= 100
+age = Math.floor(age)
+
+let pesho;           // не сме сложили
+let gosho = null;   // каваме , че няма да сложим 
+
+// експлицитно - изрично 
+// имплицитно - не е изрично
+
+// парсване = преобразуване  опитва да промени типа на данните
+
+// десен бутон Format document
+
+
+console.log('First name: ' + firstName);
+console.log('Last name: ' + lastName);
+console.log('Age: ' + age);
+
+console.log(pesho);
+console.log(gosho);
+
+ // . property = свойство - дай ми нещо!
+ // () функция = направи нещо!
+
+ console.log(Number.MAX_SAFE_INTEGER);
+ console.log(Number.MIN_SAFE_INTEGER);
+
+
+ // ` back-tick - обратен апостроф
+
+
+ let stamat = 'Stamat';
+ let evlogi = 'Evlogi';
+
+ console.log(stamat + ' ' + evlogi);
+ console.log(`${stamat} ${evlogi}`);
+
+
+ let serafim = 'serafim';
+
+ console.log(serafim);
+
+
+ console.log('5'+5+5);
+ console.log(5+5+'5');
+
+ let str = 'Q&A';
+ let a = 'a';
+
+ 
+ console.log(a.charCodeAt(0));
+ console.log(String.fromCharCode(97));
+
+
+
+let skorchev = 5 + 5 * 5; // 30
+
+console.log(skorchev);
+
+skorchev = (5 + 5) * 5; // 50
+
+console.log(skorchev);
+
+let num = 0;
+
+console.log(++num);
+console.log(num++);
+console.log(num);
+
+// как да не ми подчертава явор?
+
+// задавай въпрос на името при булева променлива
+
+let deposit = 10;
+let sumWithdraw = 5;
+let canWithdraw = deposit >= sumWithdraw;
+console.log(canWithdraw);
+
+
+let yavor = true;
+
+
+// до с
+
+
+
+
+
+
+
+
+
+
---
+++ Demos/Readme.md
@@ -0,0 +1,14 @@
+
+# My Demos from class 
+
+This is demo code files that i practice in class and that I download from course resources. 
+Some code is not finished or have good comments, because I try to do it along the mentor. 
+But this code is extremely useful for me, because I connect it with my memories from the session.
+
+
+🤯  
+🤓  
+👶🏻👨‍💻🧶  
+🎓  
+🤹🏼‍♂️
+
