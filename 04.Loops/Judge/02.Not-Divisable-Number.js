27b5e898f424ff63846b22a4e7e46bc1153dcb0b
Vladimir Petkov <vladimir.v.petkov@gmail.com>
2020-02-03 22:36:55 +0200

    Not Divisable Number

--- 04.Loops/Judge/02.Not-Divisable-Number.js
+++ 04.Loops/Judge/02.Not-Divisable-Number.js
@@ -1,13 +1,16 @@
 // Not Divisable Number
 // 02.Not-Divisable-Number.js
 
-const input = ['5'];
+const input = ['10'];
 const print = this.print || console.log;
 const gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
 const n = +gets();
 let row = '';
 for (let i = 1; i <= n; i++) {
+  if (i % 3 === 0 || i % 7 === 0) {
+    continue;
+  }
   row += `${i} `;
 }
 print(row);
---
+++ 04.Loops/Judge/Readme.md
@@ -0,0 +1,11 @@
+
+
+// Numbers From 1 to N
+
+// 01.Numbers-From-1-to-N.js
+
+
+
+// Not Divisable Number
+
+// 02.Not-Divisable-Number.js
