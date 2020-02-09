ce04dbe111408944dd3d6708b224ceea7aa14083
Vladimir Petkov <vladimir.v.petkov@gmail.com>
2020-02-09 16:20:02 +0200

    loops Demos 02 Repetition

--- 05.Arrays/Demos/02.Repetition.js
+++ 05.Arrays/Demos/02.Repetition.js
@@ -3,3 +3,15 @@
 
 let numbers = [1, 2, 3, 4, 5];
 let reversed = [];
+
+for (let i = 0; i < numbers.length; i++) {
+
+  // reversed[i] = numbers[numbers.length - i - 1]; // [5, 4, 3, 2, 1]
+  // 5-0-1 = 4
+
+  reversed[i] = numbers[numbers.length - i]; // [undefined, 5, 4, 3, 2] Защо ?
+  // 5-0 = 5 (петият index не съществува, значи е ъндефайн)
+
+}
+
+console.log(reversed);
