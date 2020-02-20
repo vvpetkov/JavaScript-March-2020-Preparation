2b83c79e578a1f2cc7d98a1cd48ac096eb45a6ad
Vladimir Petkov <vladimir.v.petkov@gmail.com>
2020-02-20 12:56:01 +0200

    Practice repetition update

---
+++ 07.Practice/relax.js
@@ -0,0 +1,11 @@
+// Relax
+// relax.js
+
+let arr24Feb = ['6', '5', '4', '3', '2', '1'];
+
+for (let i = 0; i < arr24Feb.length; i++) {
+  console.log(`day ${arr24Feb[i]} : Relax, Take a nap, Listen your favorite music!`)
+}
+
+
+console.log('%c JavaScript is cool 🛴', 'color: red; font-weight: bold; background: blue;');
---
+++ 07.Practice/repetition-video-vld.js
@@ -0,0 +1,29 @@
+// My REPETITION from Videos
+// repetition-video-vld.js
+
+
+while (true) {
+  console.log('Квартал Люлин')
+  if (true) {
+    console.log('blok 1')
+  }
+
+  if (true) {
+    console.log('blok 2')
+    break;
+  }
+
+}
+
+
+let dirtyDishes = 5;
+let cleanDishes = 0;
+
+while (dirtyDishes > 0) {
+  dirtyDishes--;
+  cleanDishes++;
+
+  console.log(`I have cleaned ${cleanDishes} and ${dirtyDishes} are left `)
+}
+
+console.log('Job ended!')
