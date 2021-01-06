a3348f45c3772df3d506d486d60194eedf2f0c59
unknown <vladimir.v.petkov@gmail.com>
2021-01-06 21:00:32 +0200

    Update 2021 Fresh and refresh

--- 04.Loops/Live Demos/00.full-demo.js
+++ 04.Loops/Live Demos/00.full-demo.js
@@ -37,3 +37,46 @@
 // }
 
 // console.log(numbers.slice(0, numbers.length - 1));
+
+// ---------- 2021
+
+// while loops
+// for loops // шугър на while
+// break
+// continue
+
+// докато е условието вярно изпълнявай цикъла
+
+let dirtyDishes = 5;
+let cleanedDishes = 0;
+
+while (dirtyDishes > 0) {
+  // dirtyDishes = dirtyDishes - 1;
+  dirtyDishes--;
+  // cleanedDishes = cleanedDishes + 1;
+  cleanedDishes++;
+  console.log(`I have cleaned ${cleanedDishes} and ${dirtyDishes} are left!`);
+}
+
+console.log("Job ended!");
+
+//
+
+let numbers = "";
+const n = 3;
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    numbers += `${i} `;
+  }
+}
+
+console.log(numbers);
+
+// continue
+for (let i = 0; i < 10; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+  console.log(i);
+}
