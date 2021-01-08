b722f9eca6a6bd5eb61bf178ed966eceafb5cc13
unknown <vladimir.v.petkov@gmail.com>
2021-01-08 16:58:23 +0200

    Проверяваме в масива сбора на четните и сбора на нечетните със Loops

--- 04.Loops/Homework Solution/08.Odd-and-Even-Product.js
+++ 04.Loops/Homework Solution/08.Odd-and-Even-Product.js
@@ -6,13 +6,13 @@ const input = ["4", "3", "2", "5", "2"];
 const print = this.print || console.log;
 const gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
-const n = +gets();
+const n = +gets(); // взима само първото число от условието
 let oddProduct = 1;
 let evenProduct = 1;
-for (let i = 1; i <= n; i++) {
-  let currentNum = +gets();
-  if (i % 2 === 1) {
-    oddProduct *= currentNum;
+for (let i = 1; i <= n; i++) {    // 
+  let currentNum = +gets();       //  взима от условието следващото
+  if (i % 2 === 1) {              //  булеан за нечетно число
+    oddProduct *= currentNum;     //  
   } else {
     evenProduct *= currentNum;
   }
@@ -26,3 +26,6 @@ if (oddProduct === evenProduct) {
   print(`odd_product=${oddProduct}`);
   print(`even_product=${evenProduct}`);
 }
+
+
+// 'Проверяваме в масива сбора на четните и сбора на нечетните със Loops'
